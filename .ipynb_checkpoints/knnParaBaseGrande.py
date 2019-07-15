7952894d08f1b418401b3be232321d7766923160
MarcosAntonioDaSilva <marcos.teci9@outlook.com>
2019-07-15 18:43:44 -0300

    Update knnParaBaseGrande.py

--- .ipynb_checkpoints/knnParaBaseGrande.py
+++ .ipynb_checkpoints/knnParaBaseGrande.py
@@ -6,11 +6,11 @@ import operator
 
 
 def carregarBancoDeDados(nomeArquivo, dividir, conjuntoTreino=[], conjuntoTeste=[]):	#Esta funação vai carregar o banco de dados
-    with open(nomeArquivo, 'rb') as csvfile:										#Também vai dividir aleatoriamente a base de dados em conjunto de treinamento e testes
+    with open(nomeArquivo, 'rb') as csvfile:	#Também vai dividir aleatoriamente a base de dados em conjunto de treinamento e testes
         linhas = csv.reader(csvfile)
         conjuntoDeDados = list(linhas)
-        for x in range(len(conjuntoDeDados) - 1):								#tamanho do conjunto de dados é a quantidade de linhas
-            for y in range(784):												#784 é o tamanho do vetor, pois cada pixel da imagem representa uma posição do vetor
+        for x in range(len(conjuntoDeDados) - 1):#tamanho do conjunto de dados é a quantidade de linhas
+            for y in range(784):	#784 é o tamanho do vetor, pois cada pixel da imagem representa uma posição do vetor
                 conjuntoDeDados[x][y] = float(conjuntoDeDados[x][y])
             if random.random() < dividir:
                 conjuntoTreino.append(conjuntoDeDados[x])
@@ -20,7 +20,7 @@ def carregarBancoDeDados(nomeArquivo, dividir, conjuntoTreino=[], conjuntoTeste=
 #Para fazer previsões, temos que calcular a similaridade entre quaisquer instâncias de dados. Utilizamos 3 medidas de distâncias descritas abaixo.
 
 def distanciaEuclidiana(instancia1, instancia2, length):	#Distância Euclidiana
-    distancia = 0										#A função vai calcular a similaridade entre as instâncias de dados
+    distancia = 0			#A função vai calcular a similaridade entre as instâncias de dados
     for x in range(length):
         distancia += pow((instancia1[x] - instancia2[x]), 2)
     return math.sqrt(distancia)
@@ -41,9 +41,9 @@ def distanciaBrayCurtis(instancia1, instancia2, length):	#Distância de Bray-Cur
 
 #Algoritmo KNN
 
-def obterVizinhos(conjuntoTreino, conjuntoTeste, k):	#Função que retorna os k-vizinhos mais similares do conjunto de treinamento para uma determinada instância de teste
-    distancias = []                             	#Calcula a distância para todas as instâncias e seleciona um subconjunto com os menores valores de distância
-    length = len(conjuntoTeste) - 1                #Função da distância Euclidiana
+def obterVizinhos(conjuntoTreino, conjuntoTeste, k):#Função que retorna os k-vizinhos mais similares do conjunto de treinamento para uma determinada instância de teste
+    distancias = []  #Calcula a distância para todas as instâncias e seleciona um subconjunto com os menores valores de distância
+    length = len(conjuntoTeste) - 1    #Função da distância Euclidiana
     for x in range(len(conjuntoTreino)):
         dist = distanciaEuclidiana(conjuntoTeste, conjuntoTreino[x], length)
         distancias.append((conjuntoTreino[x], dist))
@@ -53,8 +53,8 @@ def obterVizinhos(conjuntoTreino, conjuntoTeste, k):	#Função que retorna os k-
         vizinhos.append(distancias[x][0])
     return vizinhos
 
-def obterResposta(vizinhos):		#Função para classificar, baseadas nas repostas previstas. Obtém a maioria dos votos de respostas dada pelos vizinhos.
-    votosClasses = {}					#Cada vizinho vota em seu atributo de classe e que possui a maior quantidade de votos é escolhido
+def obterResposta(vizinhos): #Função para classificar, baseadas nas repostas previstas. Obtém a maioria dos votos de respostas dada pelos vizinhos.
+    votosClasses = {}		#Cada vizinho vota em seu atributo de classe e que possui a maior quantidade de votos é escolhido
     for x in range(len(vizinhos)):	
         resposta = vizinhos[x][-1]
         if resposta in votosClasses:
@@ -65,7 +65,7 @@ def obterResposta(vizinhos):		#Função para classificar, baseadas nas repostas 
     return votosClassificados[0][0]
 
 def obterPrecisao(conjuntoTeste, previsoes): #Função para medir a acurácia das previsões. Soma o total de previsões corretas e retorna a precisão em forma de porcentagem das classificações corretas
-    correto = 0								 #Para medir a acurácia a razão entre o total de previsões corretas sobre o total de previsões feitas
+    correto = 0			#Para medir a acurácia a razão entre o total de previsões corretas sobre o total de previsões feitas
     for x in range(len(conjuntoTeste)):			
         if conjuntoTeste[x][-1] == previsoes[x]:
             correto += 1
@@ -75,7 +75,7 @@ def main():
     #Preparar os dados
     conjuntoTreino = []
     conjuntoTeste = []
-    dividir = 0.67								#divide 33% da base de dados para teste e 67% para treino
+    dividir = 0.67				#divide 33% da base de dados para teste e 67% para treino
     carregarBancoDeDados('baseGrande.csv', dividir, conjuntoTreino, conjuntoTeste)
     print ('Conjunto de treinamento: ' + repr(len(conjuntoTreino)))
     print ('Conjunto de teste: ' + repr(len(conjuntoTeste)))
